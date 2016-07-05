5196e386e9976e68c41ff5b0e88962da7af21868
Jan Brohl <janbrohl@t-online.de>
2016-07-05 17:15:39 +0200

    add api-call for refresh

--- metadata_webtool/app.py
+++ metadata_webtool/app.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python2
 
-# not yet used
-
 # Copyright (c) 2016, Jan Brohl <janbrohl@t-online.de>
 # All rights reserved.
 
+from __future__ import unicode_literals, absolute_import
 
 import bottle
+import metadata_webtool.refresh_datajs as rd
 
 app = bottle.Bottle()
 
@@ -15,6 +15,12 @@ app = bottle.Bottle()
 @app.get("/<path:path>")
 def static(path="index.html"):
     return bottle.static_file(path, "static/metadata-webtool")
+
+
+@app.route("/api/refresh")
+def api_refresh():
+    if bottle.request.params.apikey == "changeme":
+        rd.main()
 
 if __name__ == "__main__":
     app.run()
